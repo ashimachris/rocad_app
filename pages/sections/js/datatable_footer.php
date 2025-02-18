a8c443232d2510f2878c74c3d17d1d39a79bf3ad
Christopher  Agba <agbachris555@gmail.com>
2025-02-18 06:52:27 -0800

    Add files via upload

---
+++ pages/sections/js/datatable_footer.php
@@ -0,0 +1,14 @@
+
+
+<script src="js/datatable/datatables/jquery.dataTables.min.js"></script>
+<script src="js/datatable/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="js/datatable/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="js/datatable/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="js/datatable/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="js/datatable/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="js/datatable/jszip/jszip.min.js"></script>
+<script src="js/datatable/pdfmake/pdfmake.min.js"></script>
+<script src="js/datatable/pdfmake/vfs_fonts.js"></script>
+<script src="js/datatable/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="js/datatable/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="js/datatable/datatables-buttons/js/buttons.colVis.min.js"></script>
---
+++ pages/sections/js/table.js
@@ -0,0 +1,35 @@
+$(function () {
+
+    $("#example1").DataTable();
+
+    $('#example2').DataTable({
+
+      "paging": true,
+
+      "lengthChange": false,
+
+      "searching": false,
+
+      "ordering": true,
+
+      "info": true,
+
+      "autoWidth": false
+
+    });
+
+    $("#dataTable1").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    }).buttons().container().appendTo('#dataTable1_wrapper .col-md-6:eq(0)');
+    $('#dataTable2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+    });
+
+  });
